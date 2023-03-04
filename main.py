e0606445d18422181634b48be2e0793267c62763
ViniCarvalhoP <vinimegax4@gmail.com>
2023-03-04 17:39:52 -0300

    .

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+from messages import display_messages
+
+while True:
+    resposta = input('Deseja Receber um Conselho? S?N: ')
+    if (resposta == 'S' or resposta == 's'):
+        mensagem = random.choice(display_messages)
+        print(mensagem)
+        print()
---
+++ messages.py
@@ -0,0 +1,6 @@
+display_messages = [
+    'Seja feliz :)' ,
+    'Fique tranquilo, tudo vai ficar bem!' ,
+    'olá mundo! estou aqui!' ,
+    'Acredite nos seus caminhos! Sempre!' ,
+]
